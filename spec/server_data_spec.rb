3002d54300999444c6124830a1faccb9084d1365
João Soares <jsoaresgeral@gmail.com>
2013-01-29 14:53:20 +0000

    Increase ServerData spec coverage
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- spec/server_data_spec.rb
+++ spec/server_data_spec.rb
@@ -2,22 +2,52 @@ require 'spec_helper'
 
 module Travian
   describe ServerData do
-    before(:all) do
-      fake 'tx3.travian.pt'
-      fake 'ts4.travian.de'
+    let(:klass) { ServerData }
+    let(:pt_tx3) { ServerData.new(load_server_data 'tx3.travian.pt') }
+    let(:de_ts4) { ServerData.new(load_server_data 'ts4.travian.de') }
+    let(:arabia_tx4) { ServerData.new(load_server_data 'arabiatx4.travian.com') }
+
+    describe '#world_id' do
+      it 'returns "ptx18" when called on tx3.travian.pt' do
+        pt_tx3.world_id.should == "ptx18"
+      end
+
+      it 'returns "de44" when called on ts4.travian.de' do
+        de_ts4.world_id.should == "de44"
+      end
+
+      it 'returns "sy1717" when called on arabiatx4.travian.com' do
+        arabia_tx4.world_id.should == "sy1717"
+      end
     end
 
-    let(:klass) { ServerData }
-    let(:pt_tx3_server_data) { ServerData.new(load_server_data 'tx3.travian.pt') }
-    let(:de_ts4_server_data) { ServerData.new(load_server_data 'ts4.travian.de') }
+    describe '#speed' do
+      it 'returns 3 when called on tx3.travian.pt' do
+        pt_tx3.speed.should be 3
+      end
+
+      it 'returns 1 when called on ts4.travian.de' do
+        de_ts4.speed.should be 1
+      end
+
+      it 'returns 4 when called on arabiatx4.travian.com' do
+        arabia_tx4.speed.should be 4
+      end
+    end
+
+    describe '#version' do
+      it 'returns "4.0" when called on arabiatx4.travian.pt' do
+        arabia_tx4.version.should == "4.0"
+      end
+    end
 
     describe '#restart_date' do
       it 'returns the restart time on the server restart page' do
-        de_ts4_server_data.restart_date.should == DateTime.new(2013,1,21,6,0,0,"+01:00")
+        de_ts4.restart_date.should == DateTime.new(2013,1,21,6,0,0,"+01:00")
       end
 
       it 'returns nil if the server has no restart page' do
-        pt_tx3_server_data.restart_date.should be nil
+        pt_tx3.restart_date.should be nil
       end
     end
 
