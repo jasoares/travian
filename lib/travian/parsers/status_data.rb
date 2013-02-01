c64ff3f3515a9182d3ec894207f6f6ba54400f96
João Soares <jsoaresgeral@gmail.com>
2013-02-01 18:31:59 +0000

    Refactor StatusData to fit other parsers model
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- lib/travian/parsers/status_data.rb
+++ lib/travian/parsers/status_data.rb
@@ -1,26 +1,24 @@
 module Travian
   module StatusData
-
-    class << self
+    extend self
       
-      def parse(data)
-        codes = parse_hub_codes(data)
-        codes.inject({}) do |hash,code|
-          hash[code] = parse_server_hosts(data, code)
-          hash
-        end
+    def parse(data)
+      codes = parse_hub_codes(data)
+      codes.inject({}) do |hash,code|
+        hash[code] = parse_server_hosts(data, code)
+        hash
       end
-
-      private
-
-      def parse_hub_codes(data)
-        data.css('td#link:nth-child(1)').map {|code| code.text.to_sym }
-      end
-
-      def parse_server_hosts(data, hub_code)
-        data.css("div##{hub_code} tr:nth-child(n+2) td:nth-child(2)").map(&:text)
-      end
-
     end
+
+    private
+
+    def parse_hub_codes(data)
+      data.css('td#link:nth-child(1)').map {|code| code.text.to_sym }
+    end
+
+    def parse_server_hosts(data, hub_code)
+      data.css("div##{hub_code} tr:nth-child(n+2) td:nth-child(2)").map(&:text)
+    end
+
   end
 end
