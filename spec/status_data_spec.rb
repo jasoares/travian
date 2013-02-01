7e913fdbce50b03784cf6e88f41bb67012133a5c
João Soares <jsoaresgeral@gmail.com>
2013-02-01 03:03:47 +0000

    Add StatusData module which parses data from status
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

---
+++ spec/status_data_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+module Travian
+  describe StatusData do
+    let(:data) { load_server_data 'status.travian.com' }
+    describe '.parse' do
+      subject { StatusData.parse(data) }
+
+      it { should be_a Hash }
+
+      it { should have(52).hubs }
+
+      it { should have_key :pt }
+
+    end
+
+    describe '.parse_hub_codes' do
+      it 'should be an Array' do
+        StatusData.send(:parse_hub_codes, data).should be_an Array
+      end
+
+      it 'should have 52 codes' do
+        StatusData.send(:parse_hub_codes, data).should have(52).codes
+      end
+
+      it 'should have no empty strings' do
+        StatusData.send(:parse_hub_codes, data).all? {|code| !code.empty? }
+      end
+    end
+
+    describe '.parse_server_hosts' do
+      it 'should return an array of hosts' do
+        StatusData.send(:parse_server_hosts, data, 'ae').should be_an Array
+      end
+
+      it 'should return 7 hosts when passed the hub_code "ae"' do
+        StatusData.send(:parse_server_hosts, data, 'ae').should have(7).hosts
+      end
+
+      it 'should return an empty array when passed an invalid hub_code' do
+        StatusData.send(:parse_server_hosts, data, 'fp').should have(0).hosts
+      end
+    end
+  end
+end
